2c2ba610da613a530ff9c46b33c815ec28b68b62
Abdulazeez Sodiq <70654281+Abdulzeez403@users.noreply.github.com>
2022-08-07 18:00:18 -0700

    Done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import { useEffect, useState } from "react";
 import QuoteGenerator from "./QuoteGen";
+
 
 function App() {
 
-  const [dic, setdic]=useState();
-  const fetching =()=>{
-    fetch( 'https://jsonplaceholder.typicode.com/posts/1')
-    .then(response => response.json())
-    .then(data => setdic(data))
-  }
-  useEffect(()=>{
-    fetching();
-  }, [])
 
   return (
     <div className="App">
       <QuoteGenerator/>
       
-    <h4>Data:{dic}</h4>
-    {/* <h4>Data:{dic.body}</h4> */}
-   
     </div>
   );
 }
--- src/QuoteGen.jsx
+++ src/QuoteGen.jsx
@@ -1,8 +1,31 @@
+
+import {  useState } from "react";
+
 const QuoteGenerator = () => {
+  const [dic, setdic]=useState();
+  const fetching =()=>{
+  
+    fetch( 'https://api.quotable.io/random' )
+    .then(response => response.json())
+    .then(data => {
+    // let RandowNumber = Math.floor(Math.random() * data.length);
+      setdic(data);
+    })
+
+  }
+
+    
   return ( 
     <div className="container">
-      <h4>Quote Generator!</h4>
+    <div className="content_container">
+      <h3>Quote Generator!</h3>
+      <h4>{dic?.content}</h4>
+      <h4>{dic?.dateAdded}</h4>
+    <h3>Author:{dic?.author}</h3>
+    <button onClick={()=>fetching()}>Next Quote</button>
     </div>
+    </div>
+
    );
 }
  
